20c55d13cfc0c204d3aee462e471b3c3c406e027
Noah Green <greenspaceexplorer@gmail.com>
2020-10-25 16:49:52 -0700

    StarStuff.jl now working after removing DrWatson dependency

--- src/StarStuff.jl
+++ src/StarStuff.jl
@@ -1,5 +1,30 @@
 module StarStuff
+    using Reexport
 
-greet() = print("Hello World!")
+    # external dependencies
+    @reexport using Printf
+    @reexport using Unitful
+    @reexport using UnitfulAstro
+    @reexport using UnitfulAtomic
+    @reexport using LinearAlgebra
+    @reexport using Random
+    @reexport using Distributions
+    @reexport using DifferentialEquations
+    @reexport using MultiScaleArrays
+    @reexport using GeometryBasics
+    @reexport using IterTools
+    @reexport using Measurements
 
-end # module
+    @reexport using Makie
+    @reexport using AbstractPlotting
+    @reexport using AbstractPlotting.MakieLayout
+
+    include("Particle.jl")
+    include("Kinematics.jl")    
+    include("Generator.jl")
+    include("PropUtils.jl")
+    include("ParticleTree.jl")
+    include("Bfield.jl")
+    include("RelativisticIntegrator.jl")
+    include("GUI.jl")
+end # StarStuff
